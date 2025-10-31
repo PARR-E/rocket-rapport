b94bf652b8c589b9674f0589488950ca81d49432
Jared Crow <162242567+PARR-E@users.noreply.github.com>
2025-10-31 16:08:03 -0500

    Added Player Model
    Added a model for the player rocket ship, but the material isn't working. Will likely need to re-export the dae into an fbx with Blender.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+//What does this need to share as a singleton?
+//  - Distance from starting platform (score).
+//  - Health.
+//  - Fuel.
+
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
