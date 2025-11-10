ed30c8909eff7a52a1e24337e53eff8e222d8f98
Jared Crow <162242567+PARR-E@users.noreply.github.com>
2025-11-10 16:12:29 -0600

    Basic Player Physics
    Player model has been fixed and now abides by gravity, though collision needs to be fixed.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,15 +8,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //Initial variables:
+    private Rigidbody rb;
+    Vector3 P1spd = new Vector3(0.0f , 0.0f, 0.0f);
+    Vector3 P2spd = new Vector3(0.0f , 0.0f, 0.0f);
+    float gravity = 0.10f;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Player input:
 
+
+        //Update player position, according to user input and gravity:
+        P2spd.y -= gravity;
+        rb.position += P1spd + P2spd;
+
+        rb.MovePosition(rb.position);
+
+        //On collision enter, change current position to last frame's position:
+        
     }
 }
