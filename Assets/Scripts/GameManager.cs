8474374c908e096d3d3f9e96eaf214b8b441b2c4
Jared Crow <162242567+PARR-E@users.noreply.github.com>
2025-11-14 16:04:14 -0600

    Player Movement Basics Done
    - Player now responds to input from both players,
    - Collision is more stable,
    - Singleton has been implemented with GameManager (no actions yet),
    - The camera now zooms out as the rocket gains elevation.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,38 @@
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
-    
+    //Variables:
+    public static GameManager instance;                //This is used for the singleton.
+    //public Action<int> ElevationChange;                //Service for handling when player's height changes:
+
+    //Method for being a singleton:
+    public static GameManager Instance {
+        get {
+            if (instance == null) {
+                GameObject obj = new GameObject("GameManager");
+                instance = obj.AddComponent<GameManager>();
+                DontDestroyOnLoad(obj);
+            }
+            return instance;
+        }
+    }
+    //This is also needed to be a singleton:
+    private void Awake()
+    {   
+        //Dr. Zheng's version:
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    //Subscribers:
+    /*private void OnEnable(){
+        Player.Instance.ElevationChange += UpdateCamera;        //Observer.
+    }*/
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +42,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 
 //What does this need to share as a singleton?
 //  - Distance from starting platform (score).
@@ -13,75 +14,128 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     //Initial variables:
+
     private Rigidbody rb;
     Vector3 P1spd = new Vector3(0.0f, 0.0f, 0.0f);     //XYZ values are how far each coordinate will update each frame.
     Vector3 P2spd = new Vector3(0.0f, 0.0f, 0.0f);
-    float spdMax = 4.0f;
-    float acceleration = 0.02f;
-    float deceleration = 0.005f;
+    float spdMax = 1.0f;
+    float acceleration = 0.0025f;
+    float deceleration = 0.0005f;
     float gravity = -2.0f;                       //The initial low-gravity value.
-                                                        //(The Moon's gravity is 1.62 m/s^2)
+                                                 //(The Moon's gravity is 1.62 m/s^2)
+
+    
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();                 //Initialize rigidbody variable.
-        setGravity(gravity);                     //Set the initial gravity.
+        SetGravity(gravity);                     //Set the initial gravity.
     }
-
-    // Update is called once per frame
+    
+    //Update is called once per frame with the rigidbody physics:
     void Update()
     {
-        //Player input:
+        //Player 1 input:
+        if (Input.GetKey(KeyCode.W))
+        {
+            P1spd.y -= acceleration;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            P1spd.x += acceleration;
+        }
         if (Input.GetKey(KeyCode.S))
         {
             P1spd.y += acceleration;
-            Debug.Log("S pressed!");
         }
+        if (Input.GetKey(KeyCode.D))
+        {
+            P1spd.x -= acceleration;
+        }
+        //Player 2 input:
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            P1spd.y -= acceleration;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            P1spd.x += acceleration;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            P1spd.y += acceleration;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            P1spd.x -= acceleration;
+        }
+    }
 
-        //Decrease speed values if there is gravity:
-        P1spd.x -= deceleration * 0.5f * -gravity;      //Gravity is negative!!
-        P1spd.y -= deceleration * 0.5f * -gravity;
-
+    //Handling player physics & movement. Is called once per frame with the rigidbody physics:
+    void FixedUpdate()
+    {
         //Managing speed values:
-        if (P1spd.x > spdMax)
-        {
-            P1spd.x = spdMax;
-        }
-        if (P1spd.y > spdMax)
-        {
-            P1spd.y = spdMax;
-        }
-        if (P1spd.x < 0)
-        {
-            P1spd.x = 0;
-        }
-        if (P1spd.y < 0)
-        {
-            P1spd.y = 0;
-        }
+        SpdCheck(P1spd);
+        SpdCheck(P2spd);
 
 
         //Update player position, according to user input and gravity:
         rb.position += P1spd + P2spd;
         rb.MovePosition(rb.position);
 
-        //On collision enter, change current position to last frame's position (might not be needed):
-
+        //Make the camera zoom out futher as the player gets frather from the starting area:
+        UpdateCamera(rb.position.y);
 
 
         //Debug statements:
         //Debug.Log("Player pos: (" + rb.position.x + ", " + rb.position.y + ", " + rb.position.z + ")");
         //Debug.Log("Gravity = " + Physics.gravity);
-        Debug.Log(P1spd);
+        //Debug.Log(P1spd);
+    }
 
+    //Handling collisions:
+    void OnCollisionEnter(Collision collision)
+    {
+        //Decrease the current speed:
+        P1spd = P1spd / 3;
+        P2spd = P2spd / 3;
+
+        //Log the name of the object that was collided with:
+        Debug.Log("Collided with: " + collision.gameObject.name);
+    }
+
+    void SpdCheck(Vector3 _spd)
+    {
+        if (_spd.x > spdMax)
+        {
+            _spd.x = spdMax;
+        }
+        if (_spd.y > spdMax)
+        {
+            _spd.y = spdMax;
+        }
+        if (_spd.x < -spdMax)
+        {
+            _spd.x = -spdMax;
+        }
+        if (_spd.y < -spdMax)
+        {
+            _spd.y = -spdMax;
+        }
     }
 
     //Change the rigidbody's gravity value:
-    void setGravity(float _gravity)
+    void SetGravity(float _gravity)
     {
         gravity = _gravity;
         Physics.gravity = new Vector3(0, gravity, 0);
+    }
+
+    //Make camera zoom out as the rocket travels higher:
+    void UpdateCamera(float _playerY)
+    {
+        GameManager.Instance.transform.position = new Vector3(0f, 3f, -10f - _playerY / 10);
     }
 }
